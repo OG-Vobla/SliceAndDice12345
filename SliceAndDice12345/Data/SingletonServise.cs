544c770f34d4ffa07d491a6f3ef6d21809ccfa76
Og_Vobla <gggg26705@gmail.com>
2023-05-10 15:40:29 +0300

    SingleTon

---
+++ SliceAndDice12345/Data/SingletonServise.cs
@@ -0,0 +1,15 @@
+﻿namespace SliceAndDice12345.Data
+{
+    public class SingletonServise
+    {
+        bool isAuth;
+        public bool GetUser()
+        { 
+            return isAuth;
+        }
+        public void SetUser(bool newUser)
+        {
+            isAuth = newUser;
+        }
+    }
+}
